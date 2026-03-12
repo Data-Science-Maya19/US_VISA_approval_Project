bced1151e024e1195f42fcf463ac91dcd4a89472
Data-Science-Maya19 <mayura19.zadane@gmail.com>
2026-03-12 11:57:10 -0400

    Requirements added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa_approval",
+    version="0.0.0",
+    author="Mayura",
+    author_email="mayura19.zadane@gmail.com",
+    packages=find_packages(),
+    )
